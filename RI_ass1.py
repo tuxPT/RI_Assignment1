3186ea24be05a93c24cada365e82862a1ece264c
Leonardo Costa <leonardocosta@ua.pt>
2020-10-22 19:09:22 +0100

    refactoring go static way

--- RI_ass1.py
+++ RI_ass1.py
@@ -4,23 +4,18 @@ from ass1_classes import CorpusReader, SimpleToken, ImprovedTokenizer, Indexer
 def main():
     cr = CorpusReader('all_sources_metadata_2020-03-13.csv')
     #print(cr.memory_files.keys())
-    simple_token = SimpleToken()
-    impTokenizer = ImprovedTokenizer()    
-    indexer = Indexer()
+    impTokenizer = ImprovedTokenizer()
+    files_tokens = []
     for k in cr.memory_files.keys():
-        simple_token.add_data(cr.memory_files[k],k)
-    
-
-    simple_token.sort_tokens()
+        files_tokens.append(SimpleToken.process(cr.memory_files[k]))
+        #simple_token.add_data(cr.memory_files[k],k)
+    #print(files_tokens)
     #print(simple_token.tokens)
 
-        
-
-    for token in simple_token.tokens:
-        indexer.add_token(token[0],token[1])
-
+    inverted_index = Indexer.process(files_tokens)
+    print(inverted_index)
     #print(indexer.dict_inverted_index)
 
-    
+
 
 main()
--- ass1_classes.py
+++ ass1_classes.py
@@ -1,6 +1,6 @@
 import csv
 import re
-
+import Stemmer
 
 ### Class used to read and store document data such as title and abstract
 ### Files with no title or abstract are ignored
@@ -25,26 +25,25 @@ class CorpusReader():
 
         #print(self.memory_files)
 
-   
-    
+
+
 ### Class used to create tokens
 ### Deletes non alpha numeric characters, ignores tokens with less than 3 chars, transforms all text to lower case and split
-class SimpleToken():
-    def __init__(self):
-        self.tokens = []
-        
+class SimpleToken:
 
-    def add_data(self, document_data, id_file):
+    def process(document_data):
         ### Replace all non alpha numeric characters by spaces
-        self._text = re.sub( '[^a-zA-Z0-9]+', ' ', document_data)
-         
+        _text = re.sub( '[^a-zA-Z0-9]+', ' ', document_data)
+
         ### Convert all text to lower case and split
-        self._initial_tokens = self._text.lower().split()
-        for elem in self._initial_tokens:
-            if len(elem) > 3:
-                self.tokens.append((elem, id_file))
-    
-    
+        return {token for token in _text.lower().split() if len(token)>3}
+        #print(_test)
+        #self.tokens = self._text.lower().split()
+        # for elem in self._initial_tokens:
+        #     if len(elem) > 3:
+        #         self.tokens.append((elem, id_file))
+
+
     ###Sort tokens by alphabetical order
     def sort_tokens(self):
         self.tokens = sorted(self.tokens, key= lambda x : x[0])
@@ -53,24 +52,24 @@ class SimpleToken():
 class ImprovedTokenizer(SimpleToken):
     def __init__(self):
         super()
+        self.stem = Stemmer.Stemmer('english')
         self._stop_words_file = open('snowball_stopwords_EN.txt')
         self._stop_words = self._stop_words_file.read().split()
 
 
 ### Class that given a token and a file id stores that info in a dictionary
-class Indexer():
-    def __init__(self):
-        self.dict_inverted_index = {}
-
-    def add_token(self, token, id_file):
+class Indexer:
+    def process(file_tokens):
+        index = {}
+        return {token : index.setdefault([]).append(id) for id, tokens in enumerate(file_tokens) for token in tokens}
 
         ### If token already in dictionary of inverted indexes and the file id not in the file list of the token
-        if token in self.dict_inverted_index.keys():
-            if id_file not in self.dict_inverted_index[token][1]:
-                self.dict_inverted_index[token][1].append(id_file)
-                self.dict_inverted_index[token][0] += 1
+        # if token in self.dict_inverted_index.keys():
+        #     if id_file not in self.dict_inverted_index[token][1]:
+        #         self.dict_inverted_index[token][1].append(id_file)
+        #         self.dict_inverted_index[token][0] += 1
 
-        ### If token not in dictionary of inverted indexes add token to dict and init structure with file id    
-        else:
-            self.dict_inverted_index[token] = [1,[id_file]]
+        # ### If token not in dictionary of inverted indexes add token to dict and init structure with file id
+        # else:
+        #     self.dict_inverted_index[token] = [1,[id_file]]
 
