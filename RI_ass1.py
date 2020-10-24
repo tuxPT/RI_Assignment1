5cb1e84b6a32e4623877d284fd75f86c126c5ecc
Leonardo Costa <leonardocosta@ua.pt>
2020-10-24 20:51:16 +0100

    refactoring

--- RI_ass1.py
+++ RI_ass1.py
@@ -7,65 +7,61 @@ def main():
 
     #start timer
     time1 = time.time()
-    
-    files_tokens1 = [(SimpleToken.process(cr.memory_files[k]), k) for k in cr.memory_files.keys() ]
+    simple_token = SimpleToken()
+    files_tokens1 = [(simple_token.process(cr.memory_files[k]), k) for k in cr.memory_files.keys() ]
     inverted_index1 = Indexer.process(files_tokens1)
-    
+
     #end timer
     time2 = time.time()
 
-    print('Answers')
-    print('------- with simple tokenizer -------')
-    print('a) What was the total indexing time and how much memory (roughly) is required to index this collection?')
-
-    print(str(time2 -time1) + " segundos")
-
-    print('b) What is your vocabulary size?')
-    print('R: '  + str(len(list(inverted_index1.keys()))))
-    print('c) List the ten first terms (in alphabetic order) that appear in only one document (document frequency = 1)')
+    print('Answers'
+          + '\n------- with simple tokenizer -------'
+          + '\na) What was the total indexing time and how much memory (roughly) is required to index this collection?'
+          + '\n' + str(time2 -time1) + " segundos"
+          + '\nb) What is your vocabulary size?'
+          + '\nR: ' + str(len(list(inverted_index1.keys())))
+          + '\nc) List the ten first terms (in alphabetic order) that appear in only one document (document frequency = 1)')
 
     term_with_one = [token for token in list(inverted_index1.keys()) if inverted_index1[token][0] == 1]
     ten_terms_alph = sorted(term_with_one)
     print('R: ')
     for x in ten_terms_alph[0:10]:
-        print(x  + ":" + str(inverted_index1[x])) 
+        print(x  + ":" + str(inverted_index1[x]))
     print('d) List the ten terms with highest document frequency')
     ten_terms_high = sorted(inverted_index1, key = lambda x : inverted_index1[x][0] and inverted_index1[x] , reverse= True)
     print('R: ')
     for x in ten_terms_high[0:10]:
         print( (x,inverted_index1[x][0]) )
-    
-    
-    ##start timer
+
+
+    # start timer
     time3 = time.time()
-    
-    impTokenizer = ImprovedTokenizer()
+
+    impTokenizer = ImprovedTokenizer('english', 'snowball_stopwords_EN.txt')
     files_tokens2 = [(impTokenizer.process(cr.memory_files[k]), k) for k in cr.memory_files.keys() ]
     inverted_index2 = Indexer.process(files_tokens2)
-    
-    ###stop timer 
+
+    # stop timer
     time4 = time.time()
-    
-    print('------- with improved tokenizer -------')
-    print('a) What was the total indexing time and how much memory (roughly) is required to index this collection?')
 
-    print(str(time4 -time3) + " segundos")
-
-    print('b) What is your vocabulary size?')
-    print('R: '  + str(len(list(inverted_index2.keys()))))
-    print('c) List the ten first terms (in alphabetic order) that appear in only one document (document frequency = 1)')
+    print('\n------- with improved tokenizer -------'
+    + '\na) What was the total indexing time and how much memory (roughly) is required to index this collection?'
+    + '\n' + str(time4 -time3) + " segundos"
+    + '\nb) What is your vocabulary size?'
+    + '\nR: '  + str(len(list(inverted_index2.keys())))
+    + '\nc) List the ten first terms (in alphabetic order) that appear in only one document (document frequency = 1)')
 
     term_with_one = [token for token in list(inverted_index2.keys()) if inverted_index2[token][0] == 1]
     ten_terms_alph = sorted(term_with_one)
     print('R: ')
     for x in ten_terms_alph[0:10]:
-        print(x  + ":" + str(inverted_index2[x])) 
+        print(x  + ":" + str(inverted_index2[x]))
     print('d) List the ten terms with highest document frequency')
     ten_terms_high = sorted(inverted_index2, key = lambda x : inverted_index2[x][0] and inverted_index2[x] , reverse= True)
     print('R: ')
     for x in ten_terms_high[0:10]:
         print( (x,inverted_index2[x][0]) )
-    
+
 
 
 
--- ass1_classes.py
+++ ass1_classes.py
@@ -3,92 +3,63 @@ import re
 import Stemmer
 from collections import defaultdict
 
-### Class used to read and store document data such as title and abstract
-### Files with no title or abstract are ignored
-### All data is stored in a dictionary with a format like
-### {id_file0 : "title and abstract", id_file1 : "title and abstract", id_file2 : "title and abstract"}
+# Class used to read and store document data such as title and abstract
+# Files with no title or abstract are ignored
+# All data is stored in a dictionary with a format like
+# {id_file0 : "title and abstract", id_file1 : "title and abstract", id_file2 : "title and abstract"}
 class CorpusReader():
     def __init__(self, filename):
-        ### Open csv file
+        # Open csv file
         with open(filename, "r", newline='') as csvfile:
             data_reader = csv.reader(csvfile, delimiter=',')
-            ### Pass the header line
+            # Pass the header line
             next(data_reader)
             self.memory_files = { i : line[2] + " " + line[7] for i, line in enumerate(data_reader) if line[2]  != '' and line[7] != ''}
-            #self.memory_files={}
-            #index_file=0
-            #for line in data_reader:
-                ### Store only the documents with title and abstract
-            #    if line[2]  != '' and line[7] != '':
-            #        self.memory_files[index_file] = line[2] + " " +  line[7]
-
-                ### Update file index
-            #    index_file +=1    
-
-        #print(self.memory_files)
 
 
 
-### Class used to create tokens
-### Deletes non alpha numeric characters, ignores tokens with less than 3 chars, transforms all text to lower case and split
+# Class used to create tokens
+# Deletes non alpha numeric characters, ignores tokens with less than 3 chars, transforms all text to lower case and split
 class SimpleToken:
+    def __init__(self):
+        # precompiled regex
+        self.match = re.compile('[^a-zA-Z0-9]+')
 
-    def process(document_data):
-        ### Replace all non alpha numeric characters by spaces
-        _text = re.sub( '[^a-zA-Z0-9]+', ' ', document_data)
+    def process(self, document_data):
+        # Replace all non alpha numeric characters by spaces
+        _text = self.match.sub(' ', document_data).lower().split()
 
-        ### Convert all text to lower case and split
-        return {token for token in _text.lower().split() if len(token)>3}
-        #print(_test)
-        #self.tokens = self._text.lower().split()
-        # for elem in self._initial_tokens:
-        #     if len(elem) > 3:
-        #         self.tokens.append((elem, id_file))
+        # Convert all text to lower case and split
+        return frozenset({token for token in _text if len(token)>3})
 
 
-    ###Sort tokens by alphabetical order
+    # Sort tokens by alphabetical order
     def sort_tokens(self):
         self.tokens = sorted(self.tokens, key= lambda x : x[0])
 
 
-class ImprovedTokenizer():
-    def __init__(self):
-        self.stem = Stemmer.Stemmer('english')
-        self._stop_words_file = open('snowball_stopwords_EN.txt')
-        self._stop_words = self._stop_words_file.read().split()
-    
-    def process(self,document_data):
-        ### Replace all non alpha numeric characters by spaces
-        _text = re.sub( '[^a-zA-Z0-9]+', ' ', document_data)
-        ##Filter tokens with stop words
-        token_init = [ tk for tk in _text.lower().split() if tk not in self._stop_words]
-        
-        ##Use stemm processing
-        return self.stem.stemWords(token_init)
-        ### Convert all text to lower case and split
-        #return frozenset({token for token in _text.lower().split() if len(token)>3})
-    
+class ImprovedTokenizer:
+    def __init__(self, language, stop_words_file):
+        self.stem = Stemmer.Stemmer(language)
+        self._stop_words = open(stop_words_file).read().split()
+        # precompiled regex
+        self.match = re.compile('[^a-zA-Z0-9]+')
+    def process(self, document_data):
+        # Replace all non alpha numeric characters by spaces
+        _text = self.match.sub(' ', document_data).lower().split()
 
-### Class that given a token and a file id stores that info in a dictionary
+        # Filter tokens with stop words
+        token_init = frozenset({ tk for tk in _text if tk not in self._stop_words})
+
+        # Use stemm processing
+        return self.stem.stemWords(token_init)
+
+
+# Class that given a token and a file id stores that info in a dictionary
 class Indexer:
     def process(file_tokens):
-        index = defaultdict(list)
-        for token_tuple in file_tokens:
-            for token in token_tuple[0]:
-                index[token].append(token_tuple[1])
-
-
-        inverted_index = {token : (len(index[token]), index[token]) for token in index.keys()}
-
-        #print(inverted_index)
-
-        return inverted_index
-        ### If token already in dictionary of inverted indexes and the file id not in the file list of the token
-        # if token in self.dict_inverted_index.keys():
-        #     if id_file not in self.dict_inverted_index[token][1]:
-        #         self.dict_inverted_index[token][1].append(id_file)
-        #         self.dict_inverted_index[token][0] += 1
-
-        # ### If token not in dictionary of inverted indexes add token to dict and init structure with file id
-        # else:
-        #     self.dict_inverted_index[token] = [1,[id_file]]
+        index = defaultdict(tuple)
+        for i, tokens in enumerate(file_tokens):
+            for token in tokens:
+                index[token]
+        return index
