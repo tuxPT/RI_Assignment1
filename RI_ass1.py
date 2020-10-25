806291476d4bbe625cae1dcd7bbf770fe6e31073
Leonardo Costa <leonardocosta@ua.pt>
2020-10-25 22:58:33 +0000

    cleaning, optimization and refactoring

--- RI_ass1.py
+++ RI_ass1.py
@@ -1,70 +1,30 @@
-from ass1_classes import CorpusReader, SimpleToken, ImprovedTokenizer, Indexer
+#!/bin/env python3
+from ass1_classes import CorpusReader, SimpleTokenizer, ImprovedTokenizer, Indexer, results
 import time
+import sys
 
-def main():
+data = CorpusReader.read('all_sources_metadata_2020-03-13.csv')
 
-    cr = CorpusReader('all_sources_metadata_2020-03-13.csv')
+#start timer
+time1 = time.time()
 
-    #start timer
-    time1 = time.time()
-    simple_token = SimpleToken()
-    files_tokens1 = [(simple_token.process(cr.memory_files[k]), k) for k in cr.memory_files.keys() ]
-    inverted_index1 = Indexer.process(files_tokens1)
+#if '-i' use ImprovedTokenizer
+if len(sys.argv) == 2 and sys.argv[1] == '-i':
+    tokenizer = ImprovedTokenizer('english', 'snowball_stopwords_EN.txt')
+    tokenizer_type = 'Improved'
+#else use Default SimpleTokenizer
+else:
+    tokenizer = SimpleTokenizer()
+    tokenizer_type = 'Simple'
 
-    #end timer
-    time2 = time.time()
+#Tokenization Step
+files_tokens1 = [tokenizer.process(data_from_doc) for data_from_doc in data]
+#Indexing Step
+inverted_index1 = Indexer.process(files_tokens1)
 
-    print('Answers'
-          + '\n------- with simple tokenizer -------'
-          + '\na) What was the total indexing time and how much memory (roughly) is required to index this collection?'
-          + '\n' + str(time2 -time1) + " segundos"
-          + '\nb) What is your vocabulary size?'
-          + '\nR: ' + str(len(list(inverted_index1.keys())))
-          + '\nc) List the ten first terms (in alphabetic order) that appear in only one document (document frequency = 1)')
+#end timer
+time2 = time.time()
 
-    term_with_one = [token for token in list(inverted_index1.keys()) if inverted_index1[token][0] == 1]
-    ten_terms_alph = sorted(term_with_one)
-    print('R: ')
-    for x in ten_terms_alph[0:10]:
-        print(x  + ":" + str(inverted_index1[x]))
-    print('d) List the ten terms with highest document frequency')
-    ten_terms_high = sorted(inverted_index1, key = lambda x : inverted_index1[x][0] and inverted_index1[x] , reverse= True)
-    print('R: ')
-    for x in ten_terms_high[0:10]:
-        print( (x,inverted_index1[x][0]) )
+#show results
+results(tokenizer_type, time1, time2, inverted_index1)
 
-
-    # start timer
-    time3 = time.time()
-
-    impTokenizer = ImprovedTokenizer('english', 'snowball_stopwords_EN.txt')
-    files_tokens2 = [(impTokenizer.process(cr.memory_files[k]), k) for k in cr.memory_files.keys() ]
-    inverted_index2 = Indexer.process(files_tokens2)
-
-    # stop timer
-    time4 = time.time()
-
-    print('\n------- with improved tokenizer -------'
-    + '\na) What was the total indexing time and how much memory (roughly) is required to index this collection?'
-    + '\n' + str(time4 -time3) + " segundos"
-    + '\nb) What is your vocabulary size?'
-    + '\nR: '  + str(len(list(inverted_index2.keys())))
-    + '\nc) List the ten first terms (in alphabetic order) that appear in only one document (document frequency = 1)')
-
-    term_with_one = [token for token in list(inverted_index2.keys()) if inverted_index2[token][0] == 1]
-    ten_terms_alph = sorted(term_with_one)
-    print('R: ')
-    for x in ten_terms_alph[0:10]:
-        print(x  + ":" + str(inverted_index2[x]))
-    print('d) List the ten terms with highest document frequency')
-    ten_terms_high = sorted(inverted_index2, key = lambda x : inverted_index2[x][0] and inverted_index2[x] , reverse= True)
-    print('R: ')
-    for x in ten_terms_high[0:10]:
-        print( (x,inverted_index2[x][0]) )
-
-
-
-
-
-
-main()
--- ass1_classes.py
+++ ass1_classes.py
@@ -7,20 +7,20 @@ from collections import defaultdict
 # Files with no title or abstract are ignored
 # All data is stored in a dictionary with a format like
 # {id_file0 : "title and abstract", id_file1 : "title and abstract", id_file2 : "title and abstract"}
-class CorpusReader():
-    def __init__(self, filename):
+class CorpusReader:
+    def read(filename):
         # Open csv file
         with open(filename, "r", newline='') as csvfile:
             data_reader = csv.reader(csvfile, delimiter=',')
             # Pass the header line
             next(data_reader)
-            self.memory_files = { i : line[2] + " " + line[7] for i, line in enumerate(data_reader) if line[2]  != '' and line[7] != ''}
+            return [line[2] + " " + line[7] for line in data_reader if line[7] != '']
 
 
 
 # Class used to create tokens
 # Deletes non alpha numeric characters, ignores tokens with less than 3 chars, transforms all text to lower case and split
-class SimpleToken:
+class SimpleTokenizer:
     def __init__(self):
         # precompiled regex
         self.match = re.compile('[^a-zA-Z0-9]+')
@@ -44,12 +44,13 @@ class ImprovedTokenizer:
         self._stop_words = open(stop_words_file).read().split()
         # precompiled regex
         self.match = re.compile('[^a-zA-Z0-9]+')
+        self.stem.maxCacheSize = 100000
     def process(self, document_data):
         # Replace all non alpha numeric characters by spaces
         _text = self.match.sub(' ', document_data).lower().split()
 
         # Filter tokens with stop words
-        token_init = frozenset({ tk for tk in _text if tk not in self._stop_words})
+        token_init = frozenset({ token for token in _text if token not in self._stop_words})
 
         # Use stemm processing
         return self.stem.stemWords(token_init)
@@ -58,8 +59,21 @@ class ImprovedTokenizer:
 # Class that given a token and a file id stores that info in a dictionary
 class Indexer:
     def process(file_tokens):
-        index = defaultdict(tuple)
+        index = defaultdict(list)
         for i, tokens in enumerate(file_tokens):
             for token in tokens:
-                index[token]
+                index[token].append(i)
         return index
+
+def results(tokenizer_name, time1, time2, inverted_index1):
+    print('Answers'
+          + '\n------- with ' + tokenizer_name + ' tokenizer -------'
+          + '\na) What was the total indexing time and how much memory (roughly) is required to index this collection?'
+          + '\n' + str(time2 - time1) + " segundos"
+          + '\nb) What is your vocabulary size?'
+          + '\nR: ' + str(len(inverted_index1))
+          + '\nc) List the ten first terms (in alphabetic order) that appear in only one document (document frequency = 1)'
+          + '\nR: ' + str(sorted([token for token in inverted_index1 if len(inverted_index1[token]) == 1])[0:10])
+          + '\nd) List the ten terms with highest document frequency'
+          + '\nR: ' + str(sorted([token for token in inverted_index1], key=lambda token: len(inverted_index1[token]), reverse=True)[0:10]))
+
